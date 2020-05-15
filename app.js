b38b2f79531b10550232e010086e73b49c40bd1f
Fret0lol <46795222+Fret0lol@users.noreply.github.com>
2020-05-15 23:26:10 +0200

    Opti Code

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-const burger = document.querySelector(".burger");
-const list = document.querySelector("nav ul");
-burger.addEventListener("click", function () {
-  list.classList.toggle("active");
-  burger.classList.toggle("change");
+$(".menu-collapsed").click(function() {
+  $(this).toggleClass("menu-expanded");
 });
