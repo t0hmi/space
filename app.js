c7e4731fde760aec611fcaddb3dbb2787e753527
t0hmi <59023305+t0hmi@users.noreply.github.com>
2020-05-18 08:32:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-$(".menu-collapsed").click(function() {
+/*-menu-*/
+/*$(".menu-collapsed").click(function() {
   $(this).toggleClass("menu-expanded");
+});*/
+/*-svg spaceship-*/
+const reactor = document.querySelector("#coucou");
+const textReactor = document.querySelector(".reactor");
+reactor.addEventListener("click", function(e){
+  textReactor.classList.toggle("hidden");
+  console.log("coucou");
 });
