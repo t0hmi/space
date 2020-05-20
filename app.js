b5b82125057c42d96a4f32189cac5dad07fae073
t0hmi <59023305+t0hmi@users.noreply.github.com>
2020-05-20 14:30:08 +0200

    app.js update

--- app.js
+++ app.js
@@ -3,9 +3,26 @@
   $(this).toggleClass("menu-expanded");
 });*/
 /*-svg spaceship-*/
-const reactor = document.querySelector("#coucou");
+const reactor = document.querySelector("#reacteur");
+const buttonClose = document.querySelectorAll(".button-close");
+const divSpaceship = document.querySelectorAll(".div-spaceship-content");
+const trigger = document.querySelectorAll(".trigger-spaceship");
+
+console.log(trigger);
+
 const textReactor = document.querySelector(".reactor");
 reactor.addEventListener("click", function(e){
-  textReactor.classList.toggle("hidden");
+  textReactor.classList.add("hidden");
   console.log("coucou");
 });
+
+function divSpaceshipReaveal(i){
+
+}
+function buttonCloseSection(i){
+  let section = buttonClose[i].parentNode;
+  buttonClose[i].addEventListener("click",function(e){
+  section.classList.remove("hidden");
+  });}
+
+  buttonCloseSection(0);
