1eb7ce0334827303c00b81c40c2c41357f75bbb2
t0hmi <59023305+t0hmi@users.noreply.github.com>
2020-05-28 17:07:06 +0200

    form + nav ajust

--- app.js
+++ app.js
@@ -8,27 +8,46 @@ const buttonClose = document.querySelectorAll(".button-close");
 const divSpaceship = document.querySelectorAll(".div-spaceship-content");
 const trigger = document.querySelectorAll(".trigger-spaceship");
 
-
-
-function divSpaceshipReaveal(i){
-  trigger[i].addEventListener("click",function(e){
-  divSpaceship[i].classList.add("hidden");
-  })
+function divSpaceshipReaveal(i) {
+  trigger[i].addEventListener("click", function (e) {
+    divSpaceship[i].classList.add("hidden");
+  });
 }
-function buttonCloseSection(i){
+function buttonCloseSection(i) {
   let section = buttonClose[i].parentNode;
-  buttonClose[i].addEventListener("click",function(e){
-  section.classList.remove("hidden");
-  });}
-  
-  divSpaceshipReaveal(0);
-  buttonCloseSection(0);
+  buttonClose[i].addEventListener("click", function (e) {
+    section.classList.remove("hidden");
+  });
+}
 
-  divSpaceshipReaveal(1);
-  buttonCloseSection(1);
+divSpaceshipReaveal(0);
+buttonCloseSection(0);
 
-  divSpaceshipReaveal(2);
-  buttonCloseSection(2);
+divSpaceshipReaveal(1);
+buttonCloseSection(1);
 
-  divSpaceshipReaveal(3);
-  buttonCloseSection(3);
+divSpaceshipReaveal(2);
+buttonCloseSection(2);
+
+divSpaceshipReaveal(3);
+buttonCloseSection(3);
+/*-- form --*/
+
+const btnSend = document.querySelector(".btn-send");
+const input = document.querySelectorAll(".form-div form input");
+
+btnSend.addEventListener("click", function (e) {
+  event.preventDefault();
+  input.forEach(function (element) {
+    console.log(element.value);
+    if (element.value == "") {
+      element.style.backgroundColor = "#FF3E3E";
+      element.placeholder = "require";
+      btnSend.disabled = true;
+    } else if (element.value != "") {
+      element.style.backgroundColor = "white";
+      btnSend.disabled = false;
+    }
+    btnSend.disabled = false;
+  });
+});
